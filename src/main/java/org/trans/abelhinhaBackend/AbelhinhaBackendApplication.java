384b0c2ade958f8843b8afad082e3f27e7dd77f5
Estella <estella.carvalho_fernandes@proton.me>
2025-08-25 13:06:35 -0300

    Inicio do projeto

---
+++ src/main/java/org/trans/abelhinhaBackend/AbelhinhaBackendApplication.java
@@ -0,0 +1,13 @@
+package org.trans.abelhinhaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbelhinhaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbelhinhaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/trans/abelhinhaBackend/AbelhinhaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.trans.abelhinhaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbelhinhaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
