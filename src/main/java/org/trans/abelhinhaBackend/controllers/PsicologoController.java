29ec23be8083498a8e7dee4e2b3f4544fdd8135b
Estella <estella.carvalho_fernandes@proton.me>
2025-08-25 19:27:15 -0300

    Adicionando algumas validações com spring validation

--- src/main/java/org/trans/abelhinhaBackend/controllers/PsicologoController.java
+++ src/main/java/org/trans/abelhinhaBackend/controllers/PsicologoController.java
@@ -1,10 +1,31 @@
 package org.trans.abelhinhaBackend.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.trans.abelhinhaBackend.helpers.requests.PsicologoRequest;
+import org.trans.abelhinhaBackend.helpers.responses.PsicologoResponse;
+import org.trans.abelhinhaBackend.services.entityServices.PsicologoService;
 
 @Controller
 @RequestMapping("api/psicologo")
 public class PsicologoController {
     
+    private PsicologoService psicologoService;
+
+    public PsicologoController(PsicologoService psicologoService){
+        this.psicologoService = psicologoService;
+    }
+
+    @PostMapping("cadastro")
+    public ResponseEntity<PsicologoResponse> cadastro(@RequestBody PsicologoRequest request){
+
+        
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(new PsicologoResponse());
+    }
+
 }
--- src/main/java/org/trans/abelhinhaBackend/helpers/requests/PsicologoRequest.java
+++ src/main/java/org/trans/abelhinhaBackend/helpers/requests/PsicologoRequest.java
@@ -1,16 +1,18 @@
 package org.trans.abelhinhaBackend.helpers.requests;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record PsicologoRequest(
-    String nome,
+    @NotBlank(message = "O nome é obrigatório") String nome,
     String foto,
     String descricao,
-    String crp,
+    @NotBlank(message = "O CRP é obrigatório") String crp,
     String preco,
-    Boolean atendeOnline,
+    @NotBlank(message = "Marque sim ou não para atendimento online") Boolean atendeOnline,
     String estado,
     String cidade,
-    String telefone,
-    String whatsapp,
+    @NotBlank(message = "O telefone é obrigatório") String telefone,
+    @NotBlank(message = "O whatsapp é obrigatório") String whatsapp,
     String instagram
 ) {
     
